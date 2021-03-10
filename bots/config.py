3bbd187e506e06599734e5a04f655567b6e047ae
Aaron Meaux <aaronmo21@gmail.com>
2021-03-09 21:37:19 -0600

    refactored code to not expose twitter api tokens

--- bots/config.py
+++ bots/config.py
@@ -2,19 +2,22 @@
 import tweepy
 import logging
 import os
+from dotenv import load_dotenv
 
 logger = logging.getLogger()
+load_dotenv()
+
 
 def create_api():
-    consumer_key = os.getenv("EuikJ7SURGxvAgqVnqbMe7lp6")
-    consumer_secret = os.getenv("8fvhb5cBL0KJ3qNqwEX5P8s4YKAL0xlLiq8eBooiWy8tePJxjV")
-    access_token = os.getenv("1366182774139404291-FoCqyNRfFj6HzYgmutI7PNU0zWhe1M")
-    access_token_secret = os.getenv("yoat0ZRcnMWy4JezpLprvp2WGXmygPWNoLIqOgy0NTVgK")
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_token = os.getenv('ACCESS_TOKEN')
+    access_token_secret = os.getenv('ACCESS_SECRET')
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
     try:
         api.verify_credentials()
     except Exception as e:
